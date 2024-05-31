b5d933f325cde72f696fdf5ceb43f2ab35057ab7
fancode343 <118608942+fancode343@users.noreply.github.com>
2024-05-31 11:53:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 8080;
 
 // Proxy configuration
 app.use('/', createProxyMiddleware({
-    target: 'http://de.vpsfree.es:5933/',
+    target: 'http://de.vpsfree.es:5933',
     changeOrigin: true,
     onProxyReq: (proxyReq, req, res) => {
         // Modify the request here if needed
