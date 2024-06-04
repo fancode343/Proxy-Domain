5c4f287870a2f8910f25f2b0a0c909fbea234de8
fancode343 <118608942+fancode343@users.noreply.github.com>
2024-06-04 16:53:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
-const port = 3000;
+const port = 80;
 
 // Proxy configuration
 app.use('/', createProxyMiddleware({
