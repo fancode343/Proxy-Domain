22494c1b06641fbce385b2491de6edfd6a6c45e2
fancode343 <118608942+fancode343@users.noreply.github.com>
2026-01-18 08:58:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const https = require('https');
 
 const app = express();
 const port = 8080;
-const targetUrl = 'https://google.com';
+const targetUrl = 'https://lmnet.cf';
 
 // Function to check if the target URL is online
 function checkTargetOnline(url) {
