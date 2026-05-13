52fed0e7ddf5cf777ec6918de5c0fcd85cc83370
ShinZinXU <118608942+fancode343@users.noreply.github.com>
2026-05-13 23:40:41 +0800

    Change target URL from grok.com to lmnet.cf

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const https = require('https');
 
 const app = express();
 const port = 8080;
-const targetUrl = 'https://grok.com';
+const targetUrl = 'https://lmnet.cf';
+//Testing
 
 // Function to check if the target URL is online
 function checkTargetOnline(url) {
